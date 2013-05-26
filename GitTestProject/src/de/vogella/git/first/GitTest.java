14506787c9fa1c202c8ef472d8ce6438c9602d1d
Frank Mathy <frankmathy@gmail.com>
2013-05-26 14:03:28 +0200

    Super

--- GitTestProject/src/de/vogella/git/first/GitTest.java
+++ GitTestProject/src/de/vogella/git/first/GitTest.java
@@ -6,7 +6,7 @@ public class GitTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Git is fun, but can even be better!!");
+		System.out.println("Git is fun, but can even be better!!!!!");
 	}
 
 }
